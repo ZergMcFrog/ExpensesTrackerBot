d4c0769d04d9e8a11fb352586aa1e13d2ca0c6d6
ZergMcFrog <jerome@zelek.biz>
2020-09-17 18:58:43 +0200

    added functionality to add a new user

--- ExpensesTrackerBot/bot/core.py
+++ ExpensesTrackerBot/bot/core.py
@@ -1,16 +1,17 @@
 #Module that handles the backend business logic and connects to telegram
 from telegram.ext import Updater, CommandHandler
-from ExpensesTrackerBot.database import database as db
+from ExpensesTrackerBot.database.database import Database as db
 
 def start(update, context):
-    startMessage = "I'm the ExpensesTrackerBot. I will keep track of your and your friends expenses.\n\n"
+    startMessage = "I'm the ExpensesTrackerBot. I will keep track of your expenses.\n\n"
     startMessage += "Following commands are currently avaiable:\n"
     startMessage += "/start: Replay this message\n"
-    startMessage += "/addUser <username>: Add this person to the tracked list (name must be unique)\n"
+    startMessage += "/addUser <username>: Add this person to the tracked list (name must be unique and whitespaces are not allowed)\n"
     context.bot.send_message(chat_id=update.effective_chat.id, text=startMessage)
 
 def addUser(update, context):
-    userName = " ".join(context.args)[0]
+    userInput = " ".join(context.args)
+    userName = userInput.split(" ")[0]
     database = db.getInstance()
     database.addUser(userName, False)
 
